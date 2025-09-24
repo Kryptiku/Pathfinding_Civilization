f24bb5495b405b66a0ac57429c1a0b413def04c0
Aldrich Ryan Antony <54350232+Kryptiku@users.noreply.github.com>
2025-09-25 07:52:05 +0800

    Removed captcha implementation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import './App.css';
 import PathfindingCivilization from './PathfindingCivilization/PathfindingVisualizer';
-import ReCAPTCHA from 'react-google-recaptcha';
-import axios from 'axios';
 
 function App() {
-  const recaptchaRef = useRef();
-  const [isCaptchaVerified, setIsCaptchaVerified] = useState(false); // turn to false
-
-  const handleCaptchaChange = async (token) => {
-    if (token) {
-      try {
-        const response = await axios.post('http://localhost:5000/verify-captcha', { token }); // Update the URL if needed
-        if (response.status === 200) {
-          setIsCaptchaVerified(true);
-        }
-      } catch (error) {
-        console.error('CAPTCHA verification failed:', error);
-        // Handle error (e.g., show a message to the user)
-      }
-    }
-  };
-
   return (
     <div id="container">
       <div id="header">
         <h1 id="header">Pathfinding Civilization</h1>
       </div>
-      <div class="App">
-          {!isCaptchaVerified ? (
-            <div id="captcha">
-              <h2>Please complete the CAPTCHA</h2>
-              <ReCAPTCHA id="captchashadow"
-                sitekey={process.env.REACT_APP_SITE_KEY}
-                onChange={handleCaptchaChange}
-                ref={recaptchaRef}
-              />
-            </div>
-          ) : (
-            <PathfindingCivilization />
-          )}
+      <div className="App">
+        <PathfindingCivilization />
       </div>
     </div>
   );
